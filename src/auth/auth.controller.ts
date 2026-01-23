a09ae1a4c28ba49aa54b9ed7e9cdff43a7344822
Sujoy Karmakar <sujoykr8@gmail.com>
2026-01-23 23:39:29 +0600

    feat(auth): add HTTP status code to signin endpoint in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { ApiBody } from '@nestjs/swagger';
@@ -13,6 +13,7 @@ export class AuthController {
     return this.authService.signup(dto);
   }
 
+  @HttpCode(HttpStatus.OK)
   @Post('signin')
   @ApiBody({ type: AuthDto })
   signin(@Body() dto: AuthDto) {
